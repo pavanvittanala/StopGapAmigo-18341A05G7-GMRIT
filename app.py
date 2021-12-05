833ca8c45d7b16145e0a18677a41aa7394b5648c
Pavan Vittanala <54206129+pavanvittanala@users.noreply.github.com>
2021-12-05 07:40:51 +0530

    2nd commit

--- app.py
+++ app.py
@@ -1,24 +1,48 @@
-from flask import Flask, render_template, url_for, request, Response
+from flask import Flask, render_template, url_for, request, Response,session
 from flask_mysql_connector import MySQL
-import os,glob
+# from flask_bootstrap import Bootstrap
+# from flask_nav import Nav
+# from flask_nav.elements import *
+# from dominate.tags import img
+import os
+
 
 app=Flask(__name__)
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_DATABASE'] = 'sys'
+app.config['MYSQL_DATABASE'] = 'stopgapamigo'
 mysql = MySQL(app)
+# Bootstrap(app)
+
+app.secret_key="stopgapamigo"
+# UPLOAD_FOLDER='./Uploads'
+
 
 @app.route('/')
 def home():
+	# session['user']='pavan'
+	session.pop('user',None)
 	return render_template('Home.html')
 
+
 @app.route('/user')
-def index():
+def user():
+	print(session)
 	cur = mysql.new_cursor(dictionary=True)
-	cur.execute("select column_name from information_schema.columns where table_schema='stopgapamigo' and table_name='userdetails'")
+	cur.execute("select * from userdetails")
 	output = cur.fetchall()
-	print(type(output))
-	return str(output)
-	# return render_template('User.html')
+	if session:
+		print("yes")
+	else:
+		print("No")
+	# return str(output)
+	if output:
+		print("1",output)
+		print(output[0])
+	else:
+		print("0")
+	return render_template('User.html')
+
+
 @app.route('/login',methods=['GET','POST'])
 def login():
 	if request.method == "POST":
@@ -29,23 +53,99 @@ def login():
 		cur.execute(E)
 		data=cur.fetchall()
 		return str(data)
-	return ""
+	return "d--d"
+
 
 @app.route('/register',methods=['GET','POST'])
 def register():
 	if request.method == 'POST':
-		fullname=request.form['']
-		mail=request.form['']
-		passcode=request.form['']
-		mobile=request.form['']
-		location=request.form['']
+		fullname=request.form['name']
+		mail=request.form['email']
+		passcode=request.form['password']
+		mobile=request.form['phno']
+		location=request.form['location']
 		cur=mysql.new_cursor(dictionary=True)
-		E="SELECT * from users where mailid='{0}' and passcode='{1}';".format(mail,passcode)
+		E="SELECT * from userdetails where mailid='{0}';".format(mail)
 		cur.execute(E)
 		data=cur.fetchall()
-
+		print(data)
+		if data:
+			return "User Already Exists"
 		E="INSERT INTO userdetails (fullname,location,mailid,mobile,passcode) VALUES ('{0}','{1}','{2}','{3}','{4}');".format(fullname,location,mail,mobile,passcode)
+		cur.execute(E)
+		mysql.connection.commit()
+		data=cur.fetchall()
+		print(data)
+		return "Registered Successfully"
 
 
+@app.route('/lends',methods=['GET','POST'])
+def lender():
+	if request.method == 'POST':
+		mail=request.form['email']
+		if session:
+			if session['user'] != mail:
+				session['user'] = mail
+		E="SELECT fullname from userdetails where mailid='{0}';".format(mail)
+		cur=mysql.new_cursor(dictionary=True)
+		cur.execute(E)
+		output=cur.fetchall()
+		cur.close()
+		if output:
+			fullname = output[0]['fullname']
+		else:
+			fullname = request.form['name']
+		category=request.form['category']
+		location=request.form['location']
+		descp=request.form['description']
+		cur=mysql.new_cursor(dictionary=True)
+		E="INSERT INTO lends (name,mailid,location,category,description) VALUES ('{0}','{1}','{2}','{3}','{4}');".format(fullname,mail,location,category,descp)
+		cur.execute(E)
+		mysql.connection.commit()
+		data=cur.fetchall()
+		print(data)
+		if data:
+			return "ok"
+		return "not ok	"
+
+
+@app.route('/borrows',methods=['GET','POST'])
+def borrower():
+	if request.method == 'POST':
+		mail=request.form['email']
+		if session:
+			if session['user'] != mail:
+				session['user'] = mail
+		E="SELECT fullname from userdetails where mailid='{0}';".format(mail)
+		cur=mysql.new_cursor(dictionary=True)
+		cur.execute(E)
+		output=cur.fetchall()
+		cur.close()
+		if output:
+			fullname = output[0]['fullname']
+		else:
+			fullname = request.form['name']
+		category=request.form['category']
+		location=request.form['location']
+		descp=request.form['description']
+		karmas=request.form['points']
+		cur=mysql.new_cursor(dictionary=True)
+		E="INSERT INTO lends (name,mailid,location,category,description,tender) VALUES ('{0}','{1}','{2}','{3}','{4}'.'{5}');".format(fullname,mail,location,category,descp,karma)
+		cur.execute(E)
+		mysql.connection.commit()
+		data=cur.fetchall()
+		print(data)
+		if data:
+			return "ok"
+		return "not ok	"
+
+@
+
+@app.route
+def feed():
+	pass
+
+
+# nav.init_app(app)
 if __name__ == "__main__":
 	app.run(debug=1)
--- static/js/js1.js
+++ static/js/js1.js
@@ -1,6 +1,22 @@
-function trans() {
-  
-  let reg=document.getElementsByClassName('register');
-  reg.style.backgroundColor = "blue";
+let loginDiv = document.getElementById("login-title");
+let signupDiv = document.getElementById("signup-title")
+let loginForm = document.getElementById("login-form")
+let signupForm = document.getElementById("register-form")
 
-}
+
+loginDiv.addEventListener("click", () => {
+    signupForm.style.display = "none";
+    loginForm.style.display = "block";
+    loginDiv.getElementsByTagName("p")[0].style.borderBottom = "1px solid gray"
+    signupDiv.getElementsByTagName("p")[0].style.borderBottom = "none"
+
+})
+
+signupDiv.addEventListener("click", () => {
+   
+    loginForm.style.display = "none";
+    signupForm.style.display = "block";
+    signupDiv.getElementsByTagName("p")[0].style.borderBottom = "1px solid gray"
+    loginDiv.getElementsByTagName("p")[0].style.borderBottom = "none"
+
+})
